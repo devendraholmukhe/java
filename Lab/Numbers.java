1956015c300b356d85e48526f36547496ae96082
devendraholmukhe <123162092+devendraholmukhe@users.noreply.github.com>
2023-02-02 10:23:35 -0800

    Add files via upload

---
+++ Lab/Numbers.java
@@ -0,0 +1,13 @@
+class Numbers{
+ public static void main(String args[])
+       {
+	System.out.println("\n;Divisible by 5:");
+	for(int i=5; i<=100; i=i+5){ //initialize
+	if (i%5==0)  //condition
+		System.out.println(i +", ");  //print number
+	
+	}
+	System.out.println("/n");
+}
+
+}
