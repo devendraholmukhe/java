6d81b1e62e1f9385e5670d6e2b6058a5cd628236
devendraholmukhe <123162092+devendraholmukhe@users.noreply.github.com>
2023-03-10 12:41:49 +0530

    Add files via upload

---
+++ Lab/StringBufferBuilder.java
@@ -0,0 +1,15 @@
+package string;
+
+class StringBufferBuilder{
+    public static void main(String args[])
+    {
+        StringBuffer s1 = new StringBuffer("This is StringBuffer ");
+        System.out.println(s1.append("This is a sample program"));
+        System.out.println(s1.insert(21, " object "));
+        System.out.println(s1.reverse());
+        System.out.println(s1.reverse());
+		System.out.println(s1.replace(14,20, "Builder"));
+        
+    }
+}
+
